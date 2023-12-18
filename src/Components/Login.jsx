5e4b93b6b88b33c18a1c2c9467f5e0a58b02f691
Melihcan Fidan <melihcan.fi@gmail.com>
2023-12-18 16:26:35 +0300

    Login page updates

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,23 +1,26 @@
-import { useRef } from "react";
+import { useState } from "react";
 export default function Login() {
-  const name = useRef();
-  const lastName = useRef();
-  const eMail = useRef();
-  const password = useRef();
-
-  let newUser = [
-    {
-      name: name.current,
-      lastName: lastName.current,
-      eMail: eMail.current,
-      password: password.current,
-    },
-  ];
+ const [isRegistering, setIsRegistering] = useState(false);
+  let userName = "";
+  let userLastName = "";
+  let userEMail = "";
+  let userPassword = "";
 
   function register() {
-    localStorage.setItem("users", JSON.stringify(newUser));
-  }
+    let userInfo = {
+      userName,
+      userLastName,
+      userEMail,
+      userPassword,
+    };
+    let users = [];
+    users.push(JSON.parse(localStorage.getItem("users")));
+    users.push(userInfo);
+    localStorage.setItem("users", JSON.stringify(users));
 
+
+    console.log(users);
+  }
   function login() {}
 
   function changePassword() {}
@@ -31,36 +34,37 @@ export default function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <div className="space-y-6" action="#" method="POST">
+          {isRegistering &&
           <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Ad, Soyad
-            </label>
-            <div className="mt-2 flex justify-between gap-4">
-              <input
-                ref={name}
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
-              />
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Ad, Soyad
+          </label>
+          <div className="mt-2 flex justify-between gap-4">
+            <input
+              onChange={(e) => (userName = e.target.value)}
+              id="name"
+              name="name"
+              type="text"
+              required
+              className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+            />
 
-              <input
-                ref={lastName}
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
-              />
-            </div>
+            <input
+              onChange={(e) => (userLastName = e.target.value)}
+              id="lastName"
+              name="lastName"
+              type="text"
+              required
+              className="block w-1/2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-2"
+            />
           </div>
+        </div>
+        }
+          
           <div>
             <label
               htmlFor="email"
@@ -70,7 +74,7 @@ export default function Login() {
             </label>
             <div className="mt-2">
               <input
-                ref={eMail}
+                onChange={(e) => (userEMail = e.target.value)}
                 id="email"
                 name="email"
                 type="email"
@@ -90,14 +94,14 @@ export default function Login() {
                 Password
               </label>
               <div className="text-sm">
-                <button className="font-semibold text-indigo-600 hover:text-indigo-500">
+                {isRegistering ? undefined : <button onClick={() => setIsRegistering(true)} className="font-semibold text-indigo-600 hover:text-indigo-500">
                   Şifreni mi unuttun?
-                </button>
+                </button>}
               </div>
             </div>
             <div className="mt-2">
               <input
-                ref={password}
+                onChange={(e) => (userPassword = e.target.value)}
                 id="password"
                 name="password"
                 type="password"
@@ -110,20 +114,21 @@ export default function Login() {
 
           <div>
             <button
+              onClick={ isRegistering ? register : login}
               type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Giriş yap
+              {isRegistering ? 'Üye ol' : 'Giriş Yap'}
             </button>
           </div>
-        </form>
+        </div>
 
-        <p className="mt-10 text-center text-sm text-gray-500">
+        {isRegistering ? undefined : <p className="mt-10 text-center text-sm text-gray-500">
           Üye değil misin?{" "}
           <button className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
             Üye ol.
           </button>
-        </p>
+        </p>}
       </div>
     </div>
   );
